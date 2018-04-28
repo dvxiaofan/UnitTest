c84745f8c0ff7334c3fc81efb9d7669dc39fe0c5
xiaofan <1353815633@qq.com>
2018-04-28 17:23:59 +0800

    UnitTest

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function add(x, y) {
+    var result;
+
+    if ((typeof x && typeof y) !== 'number') {
+        throw new Error('Params must be a number.');
+    }
+
+    result = x + y;
+
+    if (parseInt(result) !== result) {
+        result = parseFloat(result.toFixed(1));
+    }
+
+    return result;
+}
+console.log(`Result is ${add(0.3, 6)}`);
+
